c5cd17feffdca5b3f129e47b6a38bde5668ae9e4
Ben Zobrist <ben@zobie.com>
2020-06-18 16:18:04 -0400

    Added Xcode Files

---
+++ minesweeper/minesweeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  minesweeper
+//
+//  Created by Ben Zobrist on 6/18/20.
+//  Copyright © 2020 Ben Zobrist. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
